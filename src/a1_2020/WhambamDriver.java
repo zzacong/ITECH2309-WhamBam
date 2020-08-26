24f4d8796194537e8fbc2ddb16f9da03626f16ba
zzacong <ongzz100@gmail.com>
2020-08-27 00:23:10 +1000

    Initial Commit

---
+++ src/a1_2020/WhambamDriver.java
@@ -0,0 +1,18 @@
+/**
+WHAM BAM! Assignment Starter code for ITECH2309
+
+Please note there may be some bugs in the code as it has not been thoroughly tested.
+
+Refer to the Game Rules for the correct and expected functionality.
+**/
+
+package a1_2020;
+
+public class WhambamDriver {
+
+	public static void main(String[] args) {
+		GameEngine GE = new GameEngine();
+		GE.play();
+	}
+
+}
